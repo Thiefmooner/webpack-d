abd4dabc33f0e03eab090acccfbd61799f0db538
三重_刘德华 <moonthief@foxmail.com>
2024-11-04 13:19:09 +0800

    feature:实现简单功能，把js文件里的console，李伟杭替换成傻逼李伟杭

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import './index.css'
 import './test_scss.scss'
 //动态导入import
 import('./import_chunkFilename.js').then(
-    console.log(324)
+    //console.log(324)
 )
-console.log(" Fuck 李伟杭's Mom ")
+console.log("李伟杭我日你妈妈")
--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,10 @@ module.exports = {
                 test: /\.scss$/,
                 use: ['style-loader', 'css-loader','sass-loader']
             },
+            {
+                test: /\.js$/,
+                use:'./src/loaders/replace-loader.js'
+            }
 
         ]
     },
