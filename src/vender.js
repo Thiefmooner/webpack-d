72ae72e243d017e7b7eb7d7bb42e97c65aeea9ca
三重_刘德华 <moonthief@foxmail.com>
2024-11-05 14:20:56 +0800

    nothing fixed

--- src/vender.js
+++ src/vender.js
@@ -1,6 +1,6 @@
 function Com () {
     var div=document.createElement('div')
-    div.innerHTML="我是第二个入口文件"
+    div.innerHTML="第二个入口文件"
     return div
 }
 document.body.appendChild(Com())
--- webpack.config.js
+++ webpack.config.js
@@ -17,15 +17,15 @@ module.exports = {
 
     output: {
         path: path.resolve(__dirname, "dist"),
-        publicPath:"./",
+        publicPath: "./",
         filename: "[name].[hash:10].bundle.js",
         chunkFilename: "asset_[id:5].js"//单独打包import内容
     },
 
-    resolveLoader:{
+    resolveLoader: {
         //取个别名
-        alias:{
-            "replace-loader": path.resolve(__dirname,'./src/loaders/replace-loader')
+        alias: {
+            "replace-loader": path.resolve(__dirname, './src/loaders/replace-loader')
         }
     },
 
@@ -37,16 +37,16 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader','sass-loader']
+                use: ['style-loader', 'css-loader', 'sass-loader']
             },
             {
                 test: /\.js$/,
-                use:{
-                    loader:'replace-loader',
+                use: {
+                    loader: 'replace-loader',
                     options: {
-                        name:"野种李伟杭"//注意options参数和loader平级，在use里
+                        name: "野种李伟杭"//注意options参数和loader平级，在use里
                     }
-                }                
+                }
             }
         ]
     },
@@ -56,9 +56,9 @@ module.exports = {
             title: 'test',//如果使用了下面的template，这个title不会生效了，必须要使用templateParameters更改
             filename: 'example.html',
             template: path.resolve(__dirname, './src/index.html'),
-            templateParameters:{
-                titleName:'示例html'//这个titleName在src/index.html用模板语法接收下
-            },            
+            templateParameters: {
+                titleName: '示例html'//这个titleName在src/index.html用模板语法接收下
+            },
         })
     ],
     //devtool:'source-map',
